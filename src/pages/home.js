3f4ce22be893431a2f620f763981dddd36ad08d6
Jeroen Zwart <jeroen.zwart@live.com>
2021-03-10 22:07:44 +0100

    fetched and created basic cards

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,45 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import axios from 'axios';
+import Scream from '../components/Scream';
 
 class home extends React.Component {
+    state = {
+        screams: null
+    };
+
+    componentDidMount() {
+        axios
+            .get('/screams')
+            .then(res => {
+                console.log(res.data);
+                this.setState({
+                    screams: res.data
+                });
+            })
+            .catch(err => console.log(err));
+    }
+
     render() {
+        let recentScreamsMarkup = this.state.screams ? (
+            this.state.screams.map(scream => <Scream key={scream.screamId} scream={scream} />)
+        ) : <p>Loading ....</p>
+
         return (
         <div>
-            <h1>Home Page</h1>
+            <Grid container spacing={8}>
+                <Grid item sm={8} xs={12}>
+                    {recentScreamsMarkup}
+                </Grid>
+                <Grid item sm={4} xs={12}>
+                    <p>Profile....</p>
+                </Grid>
+            </Grid>
         </div>
         )
     }
 }
 
 export default home;
+
+// https://europe-west1-socialape-cfaf3.cloudfunctions.net/api 
